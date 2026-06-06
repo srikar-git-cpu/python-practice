a79907c0cdd875598d425e56d64c4125239eea36
srikar-git-cpu <nagubandisrikar73@gmail.com>
2026-06-06 20:33:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+weight_lbs = input('weight (lbs);')
+weight_kg = int(weight_lbs) * 0.45
+print(weight_kg)
+
+temperature_celsius = input('temperature (celsius) :')
+temperature_kelvin = int(temperature_celsius) + 273
+print(temperature_kelvin)
+
+
+num1 = int(input('enter first number: '))
+num2 = int(input('enter second number : '))
+print(num1 - num2)
+print(num1 + num2)
+print(num1 * num2)
